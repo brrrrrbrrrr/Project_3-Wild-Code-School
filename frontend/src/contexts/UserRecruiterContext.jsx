a4d67b12f00a5f80c8b7c8bb6801d7e88bb0d767
brrrrrbrrrr <benjamin.chaillan-pro@laposte.net>
2023-04-12 15:48:37 +0200

    create user context

---
+++ frontend/src/contexts/UserRecruiterContext.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/jsx-no-constructed-context-values */
+/* eslint-disable react/prop-types */
+import { useState, useContext, createContext } from "react";
+// Définir mon contexte utilisateur
+const UserContext = createContext(null);
+
+// Création du provider
+function UserProvider({ children }) {
+  const [user, setUser] = useState(null);
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
+// Création de mon hook personnalisé
+export const useUser = () => useContext(UserContext);
+// Export du provider
+export default UserProvider;
