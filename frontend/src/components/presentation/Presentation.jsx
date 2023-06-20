e62ce2fd6749aa86c4a4b4cddc99da22b82cecff
brrrrrbrrrr <benjamin.chaillan-pro@laposte.net>
2023-06-20 16:30:23 +0200

    hero done with responsive

--- frontend/src/components/presentation/Presentation.jsx
+++ frontend/src/components/presentation/Presentation.jsx
@@ -1,16 +1,42 @@
 /* eslint-disable react/self-closing-comp */
 import React from "react";
 import "./Presentation.css";
+import accueil1 from "../../assets/accueil-1.jpg";
+import accueil2 from "../../assets/accueil-2.jpg";
 
 function Presentation() {
   return (
-    <div className="container">
-      <div className="bg"></div>
+    <div className="presentation-container">
       <h1 className="title">Externatic</h1>
-      <p className="paragraph">
-        ici, chaque visage a un nom. Nos consultants ne sont pas des robots, il
-        n’y a pas d’algorithmes, de profils ou de liens automatisés{" "}
-      </p>
+      <div className="presentation-bg_container">
+        <div className="presentation-bg_column">
+          <div>
+            <div className="test">
+              <img
+                src={accueil1}
+                className="bg-accueil1"
+                alt="Femme au bord de la mer"
+              />
+
+              <p className="paragraph2">
+                Ici, chaque visage a un nom. Nos consultants ne sont pas des
+                robots, il n’y a pas d’algorithmes, de profils ou de liens
+                automatisés.
+              </p>
+            </div>
+          </div>
+
+          <img
+            src={accueil2}
+            className="bg-accueil2"
+            alt="Homme avec des chêvres"
+          />
+          <p className="paragraph1">
+            Externatic est un cabinet de recrutement informatique qui répond aux
+            vrais besoins de vraies personnes.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
