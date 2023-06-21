c65a9ef7ca190c1b4766a07c792a9733bba9f86f
OLGA\_KUZKINA\_OLGA <o.kuzkina@gmail.com>
2023-06-21 10:44:02 +0200

    fixed problem

--- frontend/src/components/notfound/NotFound.jsx
+++ frontend/src/components/notfound/NotFound.jsx
@@ -1,6 +1,12 @@
 /* eslint-disable react/function-component-definition */
+import "./NotFound.css";
+
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="notfound-container">
+      <h2 className="notfound-title">Cette page n'existe pas</h2>
+    </div>
+  );
 };
 
 export default NotFound;
