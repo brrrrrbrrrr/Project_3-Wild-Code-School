95902310ea5253ef1eb7b93c26f7a56d7a7e90a8
macaron23 <laurence.ciancia@gmail.com>
2023-06-21 10:21:13 +0200

    fix css

--- frontend/src/components/legalNotice/LegalNotice.jsx
+++ frontend/src/components/legalNotice/LegalNotice.jsx
@@ -6,7 +6,7 @@ function LegalNotice() {
     <div className="legalnotice-container">
       <h2 className="legalnotice-title">Mentions légales</h2>
       <div className="legalnotice-editor">
-        <h2>Editeur</h2>
+        <h2 className="legalnotice-h2">Editeur</h2>
         <h3>Externatic</h3>
         <p>30 rue du vin - 69000 Lyon, France</p>
         <p>Tel. 01 02 03 04 05</p>
@@ -14,7 +14,7 @@ function LegalNotice() {
         <p>Monsieur Casimir Durand, fondateur et CEO</p>
       </div>
       <div className="legalnotice-host">
-        <h2>Hébergeur</h2>
+        <h2 className="legalnotice-h2">Hébergeur</h2>
         <h3>Externatic</h3>
         <p>30 rue du vin - 69000 Lyon, France</p>
         <p>Tel. 01 02 03 04 05</p>
