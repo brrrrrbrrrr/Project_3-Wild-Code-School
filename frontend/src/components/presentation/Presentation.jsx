cca65307d791671f57b4cf7986fc3f6a00d5fb47
brrrrrbrrrr <benjamin.chaillan-pro@laposte.net>
2023-06-22 12:15:48 +0200

    fix home scroll vert

--- frontend/src/components/presentation/Presentation.jsx
+++ frontend/src/components/presentation/Presentation.jsx
@@ -6,35 +6,37 @@ import accueil2 from "../../assets/accueil-2.jpg";
 
 function Presentation() {
   return (
-    <div className="presentation-container">
-      <h1 className="title">Externatic</h1>
-      <div className="presentation-bg_container">
-        <div className="presentation-bg_column">
-          <div>
-            <div className="test">
-              <img
-                src={accueil1}
-                className="bg-accueil1"
-                alt="Femme au bord de la mer"
-              />
+    <div className="toto">
+      <div className="presentation-container">
+        <h1 className="title">Externatic</h1>
+        <div className="presentation-bg_container">
+          <div className="presentation-bg_column">
+            <div>
+              <div className="test">
+                <img
+                  src={accueil1}
+                  className="bg-accueil1"
+                  alt="Femme au bord de la mer"
+                />
 
-              <p className="paragraph2">
-                Ici, chaque visage a un nom. Nos consultants ne sont pas des
-                robots, il n’y a pas d’algorithmes, de profils ou de liens
-                automatisés.
-              </p>
+                <p className="paragraph2">
+                  Ici, chaque visage a un nom. Nos consultants ne sont pas des
+                  robots, il n’y a pas d’algorithmes, de profils ou de liens
+                  automatisés.
+                </p>
+              </div>
             </div>
-          </div>
 
-          <img
-            src={accueil2}
-            className="bg-accueil2"
-            alt="Homme avec des chêvres"
-          />
-          <p className="paragraph1">
-            Externatic est un cabinet de recrutement informatique qui répond aux
-            vrais besoins de vraies personnes.
-          </p>
+            <img
+              src={accueil2}
+              className="bg-accueil2"
+              alt="Homme avec des chêvres"
+            />
+            <p className="paragraph1">
+              Externatic est un cabinet de recrutement informatique qui répond
+              aux vrais besoins de vraies personnes.
+            </p>
+          </div>
         </div>
       </div>
     </div>
